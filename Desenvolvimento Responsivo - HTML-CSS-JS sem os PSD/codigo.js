ab0732ce0c8a5e7ed379e00fd29fc069ac44601b
10500007 <85374439+10500007@users.noreply.github.com>
2021-07-25 17:04:43 -0300

    att

--- Desenvolvimento Responsivo - HTML-CSS-JS sem os PSD/codigo.js
+++ Desenvolvimento Responsivo - HTML-CSS-JS sem os PSD/codigo.js
@@ -10,22 +10,38 @@
         console.log(menu)
        }
 
-*/
-
        /*
        function scrollTo(element){
         document.querySelector(element).scrollIntoView({behavior: "smooth"});
         }
-
         document.querySelector('.cabecalho-home__role').addEventListener("click", function(event){
         event.preventDefault();
-           
-
         scrollTo('#servicos', 1000)
-
-        
+   
 });
     */
+//------------------------------------------//
+
+//var botao_val = document.querySelector('.formulario_botao').name
+var botao = document.querySelector('input.formulario_botao')
+
+botao.addEventListener('click', valid)
+
+var nome = document.querySelector('input#nome')
+
+console.log(nome)
+ 
+ function valid(){          
+    
+     if(nome.value ==""){
+         alert("preencha o nome")
+     }
+   
+ }
+
+
+
+//----------------------///
     //ready - Executa essa função so quando todos os elementos da páginas foram carregados
   $(document).ready(function(){
     //Abrir e fechar menu com jquery //
@@ -40,6 +56,17 @@
       arrows: false,
       dots: true
     });
+
+    $('.formulario').validate({
+      rules: {
+        
+        email:{
+          required: true,
+          email: true
+        }
+      }
+    });
+    
 
 });
 //fim menu
