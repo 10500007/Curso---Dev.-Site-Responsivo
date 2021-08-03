ae123aff5cb169c8a3d54620ef32779c8ca1e26b
10500007 <85374439+10500007@users.noreply.github.com>
2021-08-03 20:28:19 -0300

    foi

--- Desenvolvimento Responsivo - HTML-CSS-JS sem os PSD/partes/cabecalho.php
+++ Desenvolvimento Responsivo - HTML-CSS-JS sem os PSD/partes/cabecalho.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>   
     <link rel="stylesheet" href="../Desenvolvimento Responsivo - HTML-CSS-JS sem os PSD/sass/estilo.css">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    
     <?php 
         $titulo = "Design Responsivo" ;        
         global $titulo_pag;
